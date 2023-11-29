33b5785798292d6d5cd7bc459a9c137cb809b205
Lander Duncan <134824437+LanderDuncan@users.noreply.github.com>
2023-11-28 20:29:14 -0500

    Moved Builder to a file-based constructor

--- WordList.java
+++ WordList.java
@@ -1,17 +1,38 @@
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class WordList extends ArrayList<Word> {
 
+    public WordList() {
+        super();
+    }
 
-    public int getCollisions(int iValue, int modValue){
+    public WordList(File f, int valuesToCalculate) {
+        Scanner wordScanner = null;
+        try {
+            wordScanner = new Scanner(f);
+        } catch (Exception e) {
+
+        }
+        while (wordScanner.hasNextLine()) {
+            this.add(new Word(wordScanner.nextLine(), valuesToCalculate));
+        }
+    }
+
+    public int getCollisions(int iValue, int modValue) {
         return -1;
     }
 
-    public long getAverageTime(int iValue){
+    public long getAverageTime(int iValue) {
         return -1;
     }
 
-    public long[] getTimeArray(int iValue){
+    public long[] getTimeArray(int iValue) {
         return null;
+    }
+
+    public void exportToCSV() {
+
     }
 }

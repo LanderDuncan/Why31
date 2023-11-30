c44709da6dddf6f91d27a6d20fc0357248503b68
Lander Duncan <134824437+LanderDuncan@users.noreply.github.com>
2023-11-29 20:35:11 -0500

    collisionCount method

--- WordList.java
+++ WordList.java
@@ -1,27 +1,58 @@
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.HashSet;
 
+/**
+ * 
+ */
 public class WordList extends ArrayList<Word> {
 
+    /**
+     * 
+     */
     public WordList() {
         super();
     }
 
+    /**
+     * 
+     * @param f
+     * @param valuesToCalculate
+     */
     public WordList(File f, int valuesToCalculate) {
         Scanner wordScanner = null;
         try {
             wordScanner = new Scanner(f);
+            while (wordScanner.hasNextLine()) {
+                this.add(new Word(wordScanner.nextLine(), valuesToCalculate));
+            }
+            wordScanner.close();
         } catch (Exception e) {
+            throw new IllegalArgumentException("File has thrown an error.");
+        }
 
-        }
-        while (wordScanner.hasNextLine()) {
-            this.add(new Word(wordScanner.nextLine(), valuesToCalculate));
-        }
     }
 
-    public int getCollisions(int iValue, int modValue) {
-        return -1;
+    /**
+     * 
+     * @param iValue
+     * @param modValue
+     * @return
+     */
+    public int CollisionCount(int iValue, int modValue) {
+        Set<Integer> claimedValues = new HashSet<Integer>();
+        int count = 0;
+        for(Word w : this){
+            Integer value = w.getHash(iValue) % modValue;
+            if(claimedValues.contains(value)){
+                count++;
+            }else {
+                claimedValues.add(value);
+            }
+        }
+        return count;
     }
 
     public long getAverageTime(int iValue) {
--- WordTest.java
+++
@@ -1,9 +0,0 @@
-public class WordTest {
-    
-    public static void main(String[] args) {
-        Word w = new Word("Hello", 32);
-        String str = new String("Hello");
-        System.out.println(w.getWord().hashCode());
-        System.out.println(str.hashCode());
-    }
-}
